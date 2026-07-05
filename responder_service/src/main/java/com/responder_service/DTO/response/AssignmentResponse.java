8037988f5b7bfa0d03f669f8c2c3c8d31344d7ea
Puneet Agnihotri <puneetagnihotri1616@gmail.com>
2026-07-06 00:21:54 +0530

    added one more response dto

---
+++ responder_service/src/main/java/com/responder_service/DTO/response/AssignmentResponse.java
@@ -0,0 +1,83 @@
+package com.responder_service.DTO.response;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.responder_service.DTO.request.AssignResponderRequest;
+import com.responder_service.Enums.Assignment_Status;
+import com.responder_service.entity.Responder;
+
+public class AssignmentResponse {
+    
+    private UUID id;
+    private UUID emergencyId;
+    private Assignment_Status status;
+    private LocalDateTime assignedAt;
+    private LocalDateTime acceptedAt;
+    private LocalDateTime reachedAt;
+    private LocalDateTime completedAt;
+    private Responder responder;
+    private AssignResponderRequest request;
+    public AssignmentResponse(){
+
+    }
+    public AssignmentResponse(AssignResponderRequest request,UUID id,UUID emergencyId,Assignment_Status status,LocalDateTime assignedAt,LocalDateTime acceptedAt,LocalDateTime reachedAt,LocalDateTime completedAt){
+        this.request=request;
+        this.id=id;
+        this.emergencyId=emergencyId;
+        this.status=status;
+        this.assignedAt=assignedAt;
+        this.acceptedAt=acceptedAt;
+        this.reachedAt=reachedAt;
+        this.completedAt=completedAt;
+    }
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public UUID getEmergencyId() {
+        return emergencyId;
+    }
+    public void setEmergencyId(UUID emergencyId) {
+        this.emergencyId = emergencyId;
+    }
+    public Assignment_Status getStatus() {
+        return status;
+    }
+    public void setStatus(Assignment_Status status) {
+        this.status = status;
+    }
+    public LocalDateTime getAssignedAt() {
+        return assignedAt;
+    }
+    public void setAssignedAt(LocalDateTime assignedAt) {
+        this.assignedAt = assignedAt;
+    }
+    public LocalDateTime getAcceptedAt() {
+        return acceptedAt;
+    }
+    public void setAcceptedAt(LocalDateTime acceptedAt) {
+        this.acceptedAt = acceptedAt;
+    }
+    public LocalDateTime getReachedAt() {
+        return reachedAt;
+    }
+    public void setReachedAt(LocalDateTime reachedAt) {
+        this.reachedAt = reachedAt;
+    }
+    public LocalDateTime getCompletedAt() {
+        return completedAt;
+    }
+    public void setCompletedAt(LocalDateTime completedAt) {
+        this.completedAt = completedAt;
+    }
+    public Responder getResponder() {
+        return responder;
+    }
+    public void setResponder(Responder responder) {
+        this.responder = responder;
+    }
+    
+}
