573b2175dfe8fa267f3fd637d4716f1c7ff97e29
Puneet Agnihotri <puneetagnihotri1616@gmail.com>
2026-07-17 10:13:15 +0530

    initialised the repositories of notification service

---
+++ notification_service/src/main/java/com/notification_service/Repository/NotificationLogRepository.java
@@ -0,0 +1,15 @@
+package com.notification_service.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.notification_service.Entity.NotificationLogs;
+
+public interface NotificationLogRepository extends JpaRepository<NotificationLogs,UUID> {
+    
+    List<NotificationLogs> findByNotificationId(UUID notificationId);
+
+    List<NotificationLogs> findByNotificationId(UUID notificationId);
+}
---
+++ notification_service/src/main/java/com/notification_service/Repository/NotificationRepository.java
@@ -0,0 +1,23 @@
+package com.notification_service.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.notification_service.Entity.Notification;
+import com.notification_service.Enums.NotificationStatus;
+
+public interface NotificationRepository extends JpaRepository<Notification,UUID> {
+    
+   List<Notification> findByReceiverId(UUID receiverId);
+   List<Notification> findByReceiverIdAndNotificationStatus(UUID receiverId,NotificationStatus status);
+
+   Page <Notification> findByReceiverId(UUID receiverId,Pageable pageable);
+
+        // Duplicate method removed; use the declaration above:
+        // List<Notification> findByReceiverIdAndNotificationStatus(UUID receiverId, NotificationStatus status);
+}
