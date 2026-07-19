9d9959ae0b108c0557dbe228440ec92b58b560ec
Puneet Agnihotri <puneetagnihotri1616@gmail.com>
2026-07-19 18:32:53 +0530

    added the events in the responder service

---
+++ notification_service/src/main/java/com/notification_service/event/Events/EmergencyPriorityUpdatedEvent.java
@@ -0,0 +1,63 @@
+package com.responder_service.event.model;
+
+import java.util.UUID;
+import com.responder_service.Enums.EmergencyType;
+import com.responder_service.Enums.Priority;
+
+public class EmergencyPriorityUpdatedEvent {
+
+    private UUID emergencyId;
+    private UUID responderId;
+    private Double latitude;
+    private Double longitude;
+    private EmergencyType emergencyType;
+    private Priority priority;
+
+    public EmergencyPriorityUpdatedEvent(){
+
+    }
+    public EmergencyPriorityUpdatedEvent(UUID emergencyId,UUID responderId,Double latitude,Double longitude,EmergencyType emergencyType,Priority priority){
+        this.emergencyId=emergencyId;
+        this.responderId=responderId;
+        this.latitude=latitude;
+        this.longitude=longitude;
+        this.emergencyType=emergencyType;
+        this.priority=priority;
+    }
+    public UUID getEmergencyId() {
+        return emergencyId;
+    }
+    public void setEmergencyId(UUID emergencyId) {
+        this.emergencyId = emergencyId;
+    }
+    public UUID getResponderId() {
+        return responderId;
+    }
+    public void setResponderId(UUID responderId) {
+        this.responderId = responderId;
+    }
+    public Double getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+    public Double getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+    public EmergencyType getEmergencyType() {
+        return emergencyType;
+    }
+    public void setEmergencyType(EmergencyType emergencyType) {
+        this.emergencyType = emergencyType;
+    }
+    public Priority getPriority() {
+        return priority;
+    }
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+}
